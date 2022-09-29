2392520370199a8d36d5f837e8bb42a9d9c7fbe3
Valentin-sanabria <valensanabria2000@hotmail.com>
2022-09-29 00:16:40 -0300

    Ex 7 - Read array back to front with pointers

--- 7-RecorrerArrayPunteros.c
+++ 7-RecorrerArrayPunteros.c
@@ -1,5 +1,6 @@
 // Crear un programa que lea un n ́umero determinado (¡100) de reales introducidos por teclado, los almacene en un vector para luego mostrarlos en orden
 // inverso Para recorrer el array deber ́a usar aritm ́etica de punteros en lugar de ́ındices del array.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,14 +10,24 @@ int main(int argc, char const *argv[])
     int i=0, j=0, tamanioArray;
     int *arrayNum;
 
-    printf("Cuantos numeros deseas ingresar?\n");
-    scanf("%d",tamanioArray);
+    printf("Cuantos numeros deseas ingresar? ");
+    scanf("%d",&tamanioArray);
 
     arrayNum = (int *) malloc(tamanioArray * sizeof(int));
 
     for(i=0; i<tamanioArray;i++){
-        
+        printf("Ingresa el numero %d ",i+1);
+        scanf("%d", &arrayNum[i]);
     }
 
+
+    printf("\nEl array normal es: \n");
+    for(i=0; i<tamanioArray;i++){
+        printf("%d, ",arrayNum[i]);
+    }
+    printf("\nEl array invertido con puntero es: \n");
+    for(i=0; i<tamanioArray;i++){
+        printf("%d, ", *(arrayNum + (tamanioArray - (i+1)) ) );
+    }
     return 0;
 }
